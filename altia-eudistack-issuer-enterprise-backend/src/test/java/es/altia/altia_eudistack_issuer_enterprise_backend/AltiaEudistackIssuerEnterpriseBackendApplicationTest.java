2217ac057e6bf9f5ad03d04ee954ad7542cb9881
Albert Rodríguez <albert.rodriguez@in2.es>
2026-02-17 16:19:22 +0100

    feat(auth): create authentic source issuance controller

--- altia-eudistack-issuer-enterprise-backend/src/test/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplicationTest.java
+++ altia-eudistack-issuer-enterprise-backend/src/test/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplicationTest.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AltiaEudistackIssuerEnterpriseBackendApplicationTest {
 
-    @SuppressWarnings("EmptyMethod")
     @Test
+    @SuppressWarnings("EmptyMethod")
     void contextLoads() {
     }
 
