6cb76d5acbef41e727b1c349360074d1d9045f3e
Alba Lopez <alba.lopez@in2.es>
2026-02-24 13:26:34 +0100

    feat: add @ConfigurationPropertiesScan annotation to enable configuration properties scanning

--- altia-eudistack-issuer-enterprise-backend/src/main/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplication.java
+++ altia-eudistack-issuer-enterprise-backend/src/main/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplication.java
@@ -2,8 +2,10 @@ package es.altia.altia_eudistack_issuer_enterprise_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AltiaEudistackIssuerEnterpriseBackendApplication {
 
     static void main(String[] args) {
