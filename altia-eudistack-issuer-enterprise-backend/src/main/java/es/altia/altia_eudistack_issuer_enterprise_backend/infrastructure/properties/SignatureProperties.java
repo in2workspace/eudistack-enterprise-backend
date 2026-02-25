4961f2bf2b1d51c02459784f52057d4476f42c41
Alba Lopez <alba.lopez@in2.es>
2026-02-25 13:11:01 +0100

    refactor: remove unnecessary ConstructorBinding annotation from SignatureProperties

--- altia-eudistack-issuer-enterprise-backend/src/main/java/es/altia/altia_eudistack_issuer_enterprise_backend/infrastructure/properties/SignatureProperties.java
+++ altia-eudistack-issuer-enterprise-backend/src/main/java/es/altia/altia_eudistack_issuer_enterprise_backend/infrastructure/properties/SignatureProperties.java
@@ -3,7 +3,6 @@ package es.altia.altia_eudistack_issuer_enterprise_backend.infrastructure.proper
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "signing")
@@ -13,7 +12,6 @@ public record SignatureProperties(
         @NotBlank @URL String coreUrl
 ) {
 
-    @ConstructorBinding
     public SignatureProperties(String provider, String coreUrl) {
         this.provider = provider;
         this.coreUrl = coreUrl;
