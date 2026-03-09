9bf8ff508cdb92b68a9407b08bd9269038510c59
Roger Miret <roger.miret@in2.es>
2026-03-09 15:13:21 +0100

    test(AltiaEudistackIssuerEnterpriseBackendApplicationTest): enable initialization test

--- altia-eudistack-issuer-enterprise-backend/src/test/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplicationTest.java
+++ altia-eudistack-issuer-enterprise-backend/src/test/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplicationTest.java
@@ -1,14 +1,14 @@
 package es.altia.altia_eudistack_issuer_enterprise_backend;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AltiaEudistackIssuerEnterpriseBackendApplicationTest {
 
-    /*@Test
-    @SuppressWarnings("EmptyMethod")
+    @Test
     void contextLoads() {
-    }*/
+    }
 
 }
 
