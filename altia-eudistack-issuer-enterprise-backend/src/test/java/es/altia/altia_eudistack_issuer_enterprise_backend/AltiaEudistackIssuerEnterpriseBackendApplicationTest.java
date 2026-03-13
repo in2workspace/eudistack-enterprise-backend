dbbf72b93b9a5fad46b804d5d2f269aa7663ff3a
Roger Miret <roger.miret@in2.es>
2026-03-13 10:35:49 +0100

    test(AltiaEudistackIssuerEnterpriseBackendApplicationTest): enable context loading test

--- altia-eudistack-issuer-enterprise-backend/src/test/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplicationTest.java
+++ altia-eudistack-issuer-enterprise-backend/src/test/java/es/altia/altia_eudistack_issuer_enterprise_backend/AltiaEudistackIssuerEnterpriseBackendApplicationTest.java
@@ -1,14 +1,15 @@
 package es.altia.altia_eudistack_issuer_enterprise_backend;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AltiaEudistackIssuerEnterpriseBackendApplicationTest {
 
-    /*@Test
+    @Test
     @SuppressWarnings("EmptyMethod")
     void contextLoads() {
-    }*/
+    }
 
 }
 
